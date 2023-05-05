94179ea09e400605bebc1ebba11a3908aade5517
ozodbek2006 <2123323wee@gmail.com>
2023-05-05 14:56:24 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function uzgar(){
+    var secondimg = document.getElementById('secondimg');
+    var uzdiv1 = document.getElementById('uzdiv1');
+    var uzdiv2 = document.getElementById('uzdiv2');
+    uzdiv2.style.border = "none"
+    uzdiv1.style.border = "2px solid orange"
+    uzdiv3.style.border="2px solid orange"
+    secondimg.setAttribute('src', "https://tinfis.uz/_next/image?url=https%3A%2F%2Fback.tinfis.uz%2Fpublic%2Fuploads%2Fall%2F2023%2F04%2F7dNwO5u2bNUBc4mkRX043Zps1oqdHNTSeZI1iMYS.jpg&w=1920&q=10")
+}
+
+function uzgar1(){
+    var secondimg = document.getElementById('secondimg');
+    var uzdiv2 = document.getElementById('uzdiv2');
+    var uzdiv1 = document.getElementById('uzdiv1');
+    uzdiv2.style.border = "2px solid orange"
+    uzdiv1.style.border = "none"
+    uzdiv3.style.border="2px solid orange"
+    secondimg.setAttribute('src', "https://tinfis.uz/_next/image?url=https%3A%2F%2Fback.tinfis.uz%2Fpublic%2Fuploads%2Fall%2F2023%2F04%2Fx0DivvJtngH8I50dhIeYXtvocJWjBjLTXAxPUAEO.jpg&w=1920&q=10")
+}
+function uzgar2(){
+    var secondimg= document.getElementById('secondimg');
+    var uzdiv2 = document.getElementById('uzdiv2');
+    var uzdiv1 = document.getElementById('uzdiv1');
+    var uzdiv3 = document.getElementById('uzdiv3') 
+    uzdiv2.style.border = "2px solid orange"
+    uzdiv1.style.border = "none"
+    uzdiv3.style.border="2px solid orange"
+    secondimg.setAttribute('src', "https://tinfis.uz/_next/image?url=https%3A%2F%2Fback.tinfis.uz%2Fpublic%2Fuploads%2Fall%2F2023%2F04%2F5mYTwDIyVSEPhYaHdjaKBVtnLunk6o6xGv6oUEgI.jpg&w=1920&q=10")
+}
